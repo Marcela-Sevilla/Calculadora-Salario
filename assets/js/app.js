6b070bc07d4df616159435444ab3b8fcfe89a83b
Marcela Sevilla <sevillamarcela514@gamil.com>
2023-02-03 12:48:47 -0500

    Guardando datos del formulario de empleados

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,9 @@
 const mostrarTipoContrato = document.getElementById('tipoContrato');
+const formEmpleados = document.getElementById('formEmpleados');
+
+const valorInput = function(input){
+    return document.getElementById(input).value;
+}
 
 mostrarTipoContrato.addEventListener('change', (e) =>{
     if(mostrarTipoContrato.value === 'planta'){
@@ -8,4 +13,23 @@ mostrarTipoContrato.addEventListener('change', (e) =>{
         document.getElementById('datosPlanta').classList.add('d-none');
         document.getElementById('datosContratista').classList.remove('d-none');
     }
+});
+
+formEmpleados.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    const documento = valorInput('documento');
+    const nombreEmpleado = valorInput('nombreEmpleado');
+    const cargoEmpleado = valorInput('cargoEmpleado');
+    const tipoContrato = valorInput('tipoContrato');
+
+    if(tipoContrato === 'planta'){
+        const valorHora = valorInput('valorHora');
+        const valorExtra = valorInput('valorExtra');
+        const deducciones = valorInput('deducciones');
+    }else{
+        const valorHora = valorInput('valorHoraContratista');
+        const horasTrabajadas = valorInput('horasTrabajadas');
+    }
+
 });
